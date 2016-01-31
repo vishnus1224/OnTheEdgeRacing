4f108827e89d8629aa9162ab6a2711f317c4f624
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-01-31 13:24:39 +0530

    added game state enum and a variable to hold the current state of the game, added method to start game and an event to broadcast game state changes

--- On The Edge Racing/Assets/Scripts/GameManager.cs
+++ On The Edge Racing/Assets/Scripts/GameManager.cs
@@ -1,37 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/**
- * Singleton class for managing the state of the game.
- * */
 public class GameManager : MonoBehaviour {
 
-	public static GameManager sharedInstance;
-
-	void Awake(){
-
-		if (sharedInstance == null) {
-		
-			sharedInstance == this;
-		
-		} else if (sharedInstance != this) {
-		
-			Destroy (gameObject);
-		}
-
-		//Do not destroy this when loading scenes.
-		DontDestroyOnLoad (gameObject);
-	
+	//represents the various states that the game can be in.
+	public enum GameState{
+		NOT_STARTED,RUNNING, PAUSED, GAME_OVER
 	}
+
+	//current state of the game.
+	private GameState currentState = GameState.NOT_STARTED;
+
+	public delegate void GameStateChanged (GameState gameState);
+
+	public static event GameStateChanged onGameStateChanged;
 
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			StartGame ();
+		}
+	}
+
+	void StartGame(){
+
+		Debug.Log ("Started");
+
+		//change the current state of the game to running.
+		currentState = GameState.RUNNING;
+
+		//send an event that the game has started.
+		if (onGameStateChanged != null) {
+			onGameStateChanged (currentState);
+		}
+
 	}
 }
