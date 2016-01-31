6051831c5c380238e72bd571c5cbf15fc805c848
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-01-31 20:00:14 +0530

    added script to detect collisions between the player and the edges of the road

---
+++ On The Edge Racing/Assets/Scripts/PlayerCollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionDetector : MonoBehaviour {
+
+	public delegate void PlayerCollided ();
+
+	public static event PlayerCollided OnPlayerCollided;
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag.Equals ("Edge")) {
+
+			if (OnPlayerCollided != null) {
+				
+				//broadcast that the player has collided.
+				OnPlayerCollided ();
+			}
+		}
+	}
+}
