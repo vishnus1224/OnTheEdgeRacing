c6e296165bc20acbd83ba6919a91afd0de3a3529
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-01-31 12:09:22 +0530

    made the game manager a singleton

--- On The Edge Racing/Assets/Scripts/GameManager.cs
+++ On The Edge Racing/Assets/Scripts/GameManager.cs
@@ -1,7 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Singleton class for managing the state of the game.
+ * */
 public class GameManager : MonoBehaviour {
+
+	public static GameManager sharedInstance;
+
+	void Awake(){
+
+		if (sharedInstance == null) {
+		
+			sharedInstance == this;
+		
+		} else if (sharedInstance != this) {
+		
+			Destroy (gameObject);
+		}
+
+		//Do not destroy this when loading scenes.
+		DontDestroyOnLoad (gameObject);
+	
+	}
+
 
 	// Use this for initialization
 	void Start () {
