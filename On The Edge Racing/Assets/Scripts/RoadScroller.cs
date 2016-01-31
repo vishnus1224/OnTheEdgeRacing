eaec479f683bf0d40cf298bf6177e23d4cdd7b4d
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-01-31 21:43:55 +0530

    created a script for scrolling the road for movement effect

---
+++ On The Edge Racing/Assets/Scripts/RoadScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadScroller : MonoBehaviour {
+
+	public float speed = 2f;
+
+	private Renderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+	
+		meshRenderer = GetComponent<Renderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		meshRenderer.material.mainTextureOffset = new Vector2 (0, Time.time * speed);
+
+	}
+}
