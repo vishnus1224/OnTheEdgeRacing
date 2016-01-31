323f095bb410f7438e78d69138971aa060db8dda
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-01-31 13:45:31 +0530

    added state checks for handling input from the user only when the game is running

--- On The Edge Racing/Assets/Scripts/PlayerInputController.cs
+++ On The Edge Racing/Assets/Scripts/PlayerInputController.cs
@@ -10,9 +10,23 @@ public class PlayerInputController : MonoBehaviour {
 		IDLE, MOVE_LEFT, MOVE_RIGHT
 	}
 
+	private GameManager.GameState currentGameState = GameManager.GameState.NOT_STARTED;
+
 	public delegate void PlayerStateChanged(PlayerState playerState);
 
 	public static event PlayerStateChanged OnPlayerStateChanged;
+
+	void OnEnable(){
+
+		GameManager.onGameStateChanged += GameStateChanged;
+
+	}
+
+	void OnDisable(){
+		
+		GameManager.onGameStateChanged -= GameStateChanged;
+
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -27,6 +41,53 @@ public class PlayerInputController : MonoBehaviour {
 
 	void LateUpdate(){
 
+		HandleGameState ();
+	}
+
+	void HandleGameState(){
+
+		switch (currentGameState) {
+
+		case GameManager.GameState.NOT_STARTED:
+
+			break;
+
+		case GameManager.GameState.RUNNING:
+
+			//process input from the user if the game is running.
+			ProcessInput ();
+
+			break;
+
+		case GameManager.GameState.PAUSED:
+
+			if (OnPlayerStateChanged != null) {
+
+				OnPlayerStateChanged (PlayerState.IDLE);
+			}
+
+			break;
+
+		case GameManager.GameState.GAME_OVER:
+
+			break;
+
+		}
+
+	}
+
+	void GameStateChanged(GameManager.GameState newState){
+
+		if (currentGameState == newState) {
+			return;
+		}
+	
+		currentGameState = newState;
+	}
+
+	void ProcessInput(){
+
+
 		if(Input.GetKeyDown(KeyCode.LeftArrow)){
 
 			if (OnPlayerStateChanged != null) {
@@ -36,12 +97,13 @@ public class PlayerInputController : MonoBehaviour {
 		}
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
-		
+
 			if (OnPlayerStateChanged != null) {
-			
+
 				OnPlayerStateChanged (PlayerState.MOVE_RIGHT);
 			}
 
 		}
+	
 	}
 }
