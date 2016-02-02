94b88a7a80a53f6b83590fcdf562c901a1802454
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-02 23:08:01 +0530

    script for moving the obstacle

---
+++ On The Edge Racing/Assets/Scripts/MoveObstacle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObstacle : MonoBehaviour {
+
+	private int movementSpeed = 4;
+
+	private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+
+		//get the rigidbody component.
+		rigidbody2D = GetComponent<Rigidbody2D> ();
+
+	}
+
+	void FixedUpdate(){
+
+		//move the obstacle downwards.
+		rigidbody2D.velocity = Vector2.down * movementSpeed;
+	}
+}
