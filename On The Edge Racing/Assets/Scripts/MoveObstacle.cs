e20febd4c28a176fabc38cd9421e35de3e0fed1f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-02 23:11:28 +0530

    re factored variable name

--- On The Edge Racing/Assets/Scripts/MoveObstacle.cs
+++ On The Edge Racing/Assets/Scripts/MoveObstacle.cs
@@ -5,19 +5,19 @@ public class MoveObstacle : MonoBehaviour {
 
 	private int movementSpeed = 4;
 
-	private Rigidbody2D rigidbody2D;
+	private Rigidbody2D blockRigidbody2D;
 
 	// Use this for initialization
 	void Start () {
 
 		//get the rigidbody component.
-		rigidbody2D = GetComponent<Rigidbody2D> ();
+		blockRigidbody2D = GetComponent<Rigidbody2D> ();
 
 	}
 
 	void FixedUpdate(){
 
 		//move the obstacle downwards.
-		rigidbody2D.velocity = Vector2.down * movementSpeed;
+		blockRigidbody2D.velocity = Vector2.down * movementSpeed;
 	}
 }
